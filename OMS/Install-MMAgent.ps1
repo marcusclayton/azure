1a83527b35e98eb61d0cac08575fddc1eb5a04d0
Marcus <marcusaclayton@gmail.com>
2016-10-20 13:41:55 -0700

    Updating OMS folder and files.

---
+++ OMS/Install-MMAgent.ps1
@@ -0,0 +1,32 @@
+Configuration MMAgent
+{
+    $OIPackageLocalPath = "C:\MMASetup-AMD64.exe"
+    $OPSINSIGHTS_WS_ID = Get-AutomationVariable -Name "OPSINSIGHTS_WS_ID"
+    $OPSINSIGHTS_WS_KEY = Get-AutomationVariable -Name "OPSINSIGHTS_WS_KEY"
+
+
+    Import-DscResource -ModuleName xPSDesiredStateConfiguration
+
+    Node OMSnode {
+        Service OIService
+        {
+            Name = "HealthService"
+            State = "Running"
+            DependsOn = "[Package]OI"
+        }
+
+        xRemoteFile OIPackage {
+            Uri = "http://download.microsoft.com/download/0/C/0/0C072D6E-F418-4AD4-BCB2-A362624F400A/MMASetup-AMD64.exe"
+            DestinationPath = $OIPackageLocalPath
+        }
+
+        Package OI {
+            Ensure = "Present"
+            Path  = $OIPackageLocalPath
+            Name = "Microsoft Monitoring Agent"
+            ProductId = "8A7F2C51-4C7D-4BFD-9014-91D11F24AAE2"
+            Arguments = '/C:"setup.exe /qn ADD_OPINSIGHTS_WORKSPACE=1 OPINSIGHTS_WORKSPACE_ID=' + $OPSINSIGHTS_WS_ID + ' OPINSIGHTS_WORKSPACE_KEY=' + $OPSINSIGHTS_WS_KEY + ' AcceptEndUserLicenseAgreement=1"'
+            DependsOn = "[xRemoteFile]OIPackage"
+        }
+    }
+} 
---
+++ OMS/OnboardVMToAzureAutomationDSC.ps1
@@ -0,0 +1,12 @@
+﻿# Define the parameters for Get-AzureRmAutomationDscOnboardingMetaconfig using PowerShell Splatting
+$Params = @{
+    ResourceGroupName = 'Automation'; # The name of the ARM Resource Group that contains your Azure Automation Account
+    AutomationAccountName = 'Automation'; # The name of the Azure Automation Account where you want a node on-boarded to
+    ComputerName = @('mdt01', 'scorch01','aadsc01.lab.cloudcents.net'); # The names of the computers that the meta configuration will be generated for
+    OutputFolder = "C:\Automation\AA-DSC\";
+}
+
+# Use PowerShell splatting to pass parameters to the Azure Automation cmdlet being invoked
+# For more info about splatting, run: Get-Help -Name about_Splatting
+Login-AzureRmAccount
+Get-AzureRmAutomationDscOnboardingMetaconfig @Params -Force
