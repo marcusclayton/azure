18e3984182857c620c11a0ae395e902a67c6709a
Marcus <marcusaclayton@gmail.com>
2016-10-26 00:39:42 -0700

    Added ps1 which demonstrates the use of classes in PS v5.

---
+++ NewThings/UsingClassesInV5.ps1
@@ -0,0 +1,25 @@
+﻿Class Something {
+
+    $name
+    $date
+    $color = "cyan"
+    $type
+
+    [void]Delete() {
+
+        Write-Host "Deleting the thing." `
+        -foregroundcolor $this.color
+
+    }
+}
+
+$new = [something]::new()
+$new.Delete()
+$new.color = "green"
+$new.Delete()
+
+$new.name = "Marcus"
+$new.date = get-date -Format "MMddyyy"
+$new.type = "new"
+$new
+$new | gm 
