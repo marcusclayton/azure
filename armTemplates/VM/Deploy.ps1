52195d42bcd40d55ee89c08951d611f3584c6af1
Marcus <marcusaclayton@gmail.com>
2016-12-12 13:55:39 -0800

    created new deployment script.

---
+++ armTemplates/VM/Deploy.ps1
@@ -0,0 +1,17 @@
+$secret = Get-AzureKeyVaultSecret -VaultName temp -SecretName adminpasswd
+$TemplateURI = "https://raw.githubusercontent.com/marcusclayton/azure/master/armTemplates/VM/azuredeploy.json"
+$resourceGroup = "armkeyvaulttest001"
+$resourceGroupLocation = "West US"
+
+$Params = @{
+    pipvm001DnsName = "mcautodev04747" ;
+    uniqueprefix = "1432vms";
+    myvm001Name = "vm-dev-0001";
+    myvm001AdminUserName = "marcus";
+    myvm001WindowsOSVersion = "2016-Datacenter-with-Containers"; 
+    adminpasswd = $secret.SecretValue;
+}
+
+$rg = New-AzureRmResourceGroup -Name $Params.rg -Location $Params.rgLocation -Verbose -Force -ErrorAction Stop
+
+$deployment = New-AzureRmResourceGroupDeployment -Name "AzureDeployment-$(get-date -Format "yyyy-dd-mm-hh-mm-ss")" -Mode Incremental -TemplateParameterObject $Params -ResourceGroupName $resourceGroup -TemplateUri $TemplateURI -ErrorVariable deploymentError -Verbose -Force
