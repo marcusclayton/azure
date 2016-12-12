ff0757a99344ebeb96328af9eb77ed868ffb340f
Marcus <marcusaclayton@gmail.com>
2016-12-12 15:15:03 -0800

    Updated deployment script.

--- armTemplates/VM/Deploy.ps1
+++ armTemplates/VM/Deploy.ps1
@@ -2,14 +2,17 @@ $secret = Get-AzureKeyVaultSecret -VaultName temp -SecretName adminpasswd
 $TemplateURI = "https://raw.githubusercontent.com/marcusclayton/azure/master/armTemplates/VM/azuredeploy.json"
 $resourceGroup = "armkeyvaulttest001"
 $resourceGroupLocation = "West US"
+$backupPolicy = (Get-AzureRmRecoveryServicesBackupProtectionPolicy).Name | out-gridview -title "Select a Backup Protection Policy..." -PassThru
 
 $Params = @{
     pipvm001DnsName = "mcautodev04747" ;
     uniqueprefix = "1432vms";
     myvm001Name = "vm-dev-0001";
     myvm001AdminUserName = "marcus";
-    myvm001WindowsOSVersion = "2016-Datacenter-with-Containers"; 
-    adminpasswd = $secret.SecretValue;
+    myvm001WindowsOSVersion = "2016-Datacenter-with-Containers";
+    adminpasswd = $secret.SecretValue ;
+    tagCostCenter = "IT" ;
+    VMBackupPolicy = $backupPolicy ;
 }
 
 $rg = New-AzureRmResourceGroup -Name $Params.rg -Location $Params.rgLocation -Verbose -Force -ErrorAction Stop
