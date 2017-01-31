4b57667444ec012d26c03746d6d451fc4e0582e8
Marcus <marcusaclayton@gmail.com>
2017-01-31 02:18:11 -0800

    Added snippet to prompt for ARM deployment input

---
+++ Management/Get-ARMInputFromUser.ps1
@@ -0,0 +1,22 @@
+﻿$location = 'southcentralus'
+$i = 0
+$images = Get-AzureRmVmSize -Location $location | where {($_.Name -match "DS") -and ($_.Name -match "_v2")}
+
+$hash = $images | %{
+    $hash = @{}
+    $hash[$i] = ($_ | select name,@{l='NumCores';e={$_.NumberOfCores}},@{l="MemoryGB";e={$_.MemoryInMB/1024}})
+    $hash
+    $i++
+}
+
+$hash
+
+Write-Host -NoNewline "Enter the index number of the VM Image to use: "  -ForegroundColor yellow; $index = read-host 
+
+#Display the chosen location
+$selection = $hash[$index].values.Name
+Write-Host "Selected VM Image is: " $selection -ForegroundColor Green
+
+
+
+#Now do something with the string $selection ....
